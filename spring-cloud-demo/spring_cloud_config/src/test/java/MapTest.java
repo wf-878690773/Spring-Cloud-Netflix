0ac0c7d7a0a61e9f7f050b45e9715a0ea5db2912
wf <wf950827@163.com>
2020-01-19 20:37:29 +0800

    123

---
+++ spring-cloud-demo/spring_cloud_config/src/test/java/MapTest.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap hashMap = new ConcurrentHashMap();
+        hashMap.
+    }
+}
